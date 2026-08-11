c2ae644c0e1bb19bcf5be57040a918fe4004880c
matheuskovaliskioliveira-bit <matheus.kovaliski.oliveira@escola.pr.gov.br>
2026-08-11 18:49:58 -0300

    Add files via upload

---
+++ Aula 1/ex1.py
@@ -0,0 +1,4 @@
+colegas = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
+
+print(colegas[0]) 
+print(colegas[-1])  
---
+++ Aula 1/ex4.py
@@ -0,0 +1,8 @@
+pares = []
+
+for numero in range(1, 21):
+    if numero % 2 == 0:
+        pares.append(numero)
+
+print(pares)
+print(len(pares))
